344142d0b66a5511f545a08ee6cc7e11abea7c69
Derek Shanks <dbshanks75@hotmail.com>
2020-09-11 13:54:25 -0300

    Converting files from a more up to date version of this package.

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
-const hello = (name) => {
-    return console.log(`Hello ${ name }`);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  function hello(name) {
+    return console.log("Hello" + name);
+  }
 
-hello('World');
+  hello(" World");
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,71 @@
-const {
-  src,
-  dest,
-  watch,
-  series,
-  parallel
-} = require('gulp');
-const babel = require('gulp-babel');
-const autoprefixer = require('autoprefixer');
+const { src, dest, watch, series, parallel } = require("gulp");
+const babel = require("gulp-babel");
+const autoprefixer = require("autoprefixer");
 const rename = require("gulp-rename");
-const cssnano = require('cssnano');
-const concat = require('gulp-concat');
-const postcss = require('gulp-postcss');
-const responsivefont = require('postcss-responsive-font');
-const replace = require('gulp-replace');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
+const cssnano = require("cssnano");
+const concat = require("gulp-concat");
+const postcss = require("gulp-postcss");
+const replace = require("gulp-replace");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const browserSync = require("browser-sync").create();
 
 const files = {
-  scssPath: 'app/scss/**/*.scss',
-  jsPath: 'app/js/**/*.js'
-}
+  scssPath: "app/scss/**/*.scss",
+  jsPath: "app/js/**/*.js",
+};
 
 function scssTask() {
   return src(files.scssPath)
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(postcss([responsivefont(), autoprefixer(), cssnano()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist/css'))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/css"))
     .pipe(browserSync.stream());
 }
 
 function jsTask() {
   return src(files.jsPath)
-    .pipe(babel())
-    .pipe(concat('app.js'))
-    .pipe(rename({
-      suffix: ".min"
-    }))
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(concat("app.js"))
     .pipe(uglify())
-    .pipe(dest('dist/js'));
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+
+    .pipe(dest("dist/js"));
 }
 
 const cbString = new Date().getTime();
 
 function cacheBustTask() {
-  return src(['index.html'])
-    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-    .pipe(dest('.'));
+  return src(["index.html"])
+    .pipe(replace(/cb=\d+/g, "cb=" + cbString))
+    .pipe(dest("."));
 }
-
 
 function watchTask() {
   browserSync.init({
     server: {
-      baseDir: './'
-    }
+      baseDir: "./",
+    },
   });
-  watch([files.scssPath, files.jsPath],
-    parallel(scssTask, jsTask));
-  watch('./**/*.html').on('change', browserSync.reload);
-  watch(files.jsPath).on('change', browserSync.reload);
+  watch([files.scssPath, files.jsPath], parallel(scssTask, jsTask));
+  watch("./**/*.html").on("change", browserSync.reload);
+  watch(files.jsPath).on("change", browserSync.reload);
 }
 
-exports.default = series(
-  parallel(scssTask, jsTask),
-  cacheBustTask,
-  watchTask
-);
+exports.default = series(parallel(scssTask, jsTask), cacheBustTask, watchTask);
